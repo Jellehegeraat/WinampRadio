be8847fa1f225367e58e8e916f9ba67e4709c7f0
jellehegeraat <jelle_hegeraat@hotmail.com>
2017-06-26 14:32:22 +0200

    test

---
+++ src/install.js
@@ -0,0 +1,17 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'WinampRadio',
+  description: 'WinampRadio http://localhost:4422',
+  script: __dirname + '/app.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+
+/*svc.on('install',function(){
+  svc.start();
+});
+*/
+svc.install();

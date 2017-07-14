d4da3b33da70dbc8a6f162def05cd86d3f16cae2
jellehegeraat <jelle_hegeraat@hotmail.com>
2017-07-14 09:05:04 +0200

    Time-out-button

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     player : {
         newStationTimeoutMinutes: 20,             
-		restoreTimeoutMinutes: 3,
+		restoreTimeoutMinutes: 60,
 		winampLocation: '"C:\\Program Files (x86)\\winamp\\winamp.exe"',
     }
 };
